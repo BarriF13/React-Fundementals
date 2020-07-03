7733a3626feefdc7f0c600f513e31e148359d81f
Barri <info@bdesignstudio.co.uk>
2020-07-03 16:59:37 +0100

    Class

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,43 @@
-import React from 'react';
+//--------sample functional component which they only returning something 
+// import React from 'react';
 
-const getYear =()=>{
- const newDate = new Date();
-return newDate.getFullYear();
-} 
-const user = {
-  name: 'Barri',
-  LastName: 'Clever'
+// const getYear =()=>{
+//  const newDate = new Date();
+// return newDate.getFullYear();
+// } 
+// const user = {
+//   name: 'Barri',
+//   LastName: 'Clever'
+// }
+//---------------main job to do is to return in this example
+// const Header = () => {
+// return ( 
+//   <div>
+//     <div> The year is {getYear()}</div>
+//     <div>My name is {user.name} {user.LastName}</div>
+//   </div>
+// )
+// }
+// export default Header;
+
+//--------in example below our function needs to know more than just returning so 1--we need a class component
+//2-- after making a class we need to connect it to react so we extends it to react.component
+//3-- we need to render the class--render function 
+//4-- now we need to destructuring so we ass Component on the import line
+import React,{Component} from 'react';
+
+
+
+class Header extends Component {
+    render() {
+      return ( 
+        <header>
+          <div>Logo</div>
+          <input type="text" />
+        </header>
+      )
+      }
 }
 
-const Header = () => {
-return ( 
-  <div>
-    <div> The year is {getYear()}</div>
-    <div>My name is {user.name} {user.LastName}</div>
-  </div>
-)
-}
+
 export default Header;
