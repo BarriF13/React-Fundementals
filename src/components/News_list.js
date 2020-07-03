9709b1525ca5379c6f689a935b49a4ae0781093d
Barri <info@bdesignstudio.co.uk>
2020-07-03 19:32:42 +0100

    Add and print the props

--- src/components/News_list.js
+++ src/components/News_list.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 //information from other places coming as other props in the () below
 const NewsList = ( props ) => {
-console.log(props.dog);
+// console.log(props.dog);
+
+  const items = props.news.map((item) => {
+
+    return (
+      <div>
+        <h3>{item.title}</h3>
+        <div>
+          {item.feed}
+        </div>
+      </div>
+    )
+  }); 
   return (
-    <div>News List</div>
+    <Fragment>
+      {/* <div>{props.news}</div> --wont show because it is an array of object */}
+      {items}
+    </Fragment>
   )
 }
 
