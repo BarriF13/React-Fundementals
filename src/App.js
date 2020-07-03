c54b83cc82782da5e1373417c4ff8beda3735776
Barri <info@bdesignstudio.co.uk>
2020-07-03 19:04:24 +0100

    Prop up between component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import NewsList from './components/News_list'
 import JSON from './db.json';
 import './App.css';
 
-class App extends Component() {
+class App extends Component {
+
+  state = {
+    news:JSON
+  }
  
   render(){
+    // console.log(this.state.news);
     return (
    
       <div className="App">
       <Header />
+      <NewsList news={this.state.news} dog="zaghart"/>
      </div>  
  
     );
--- src/components/News_list.js
+++ src/components/News_list.js
@@ -1,1 +1,11 @@
-list
+import React from 'react';
+
+//information from other places coming as other props in the () below
+const NewsList = ( props ) => {
+console.log(props.dog);
+  return (
+    <div>News List</div>
+  )
+}
+
+export default NewsList;
