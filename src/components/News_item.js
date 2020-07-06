004ff87410ae7fc89b304fa24a70fe30679c5503
Barri <info@bdesignstudio.co.uk>
2020-07-06 16:42:45 +0100

    Ready for eject

--- src/components/News_item.js
+++ src/components/News_item.js
@@ -1,25 +1,30 @@
 import React from 'react';
 
-import { css } from 'glamor';
+// import { css } from 'glamor';
+import classes from '../css/styles.css'
+
+
 //we need to say to items below to use the props = data which we grab from other component so we use prop.item.title or rename props with item-- it is like saying ftom the props which is a big object I only want the item 
 export const NewsItem = ({item}) => {
 // console.log(props);
 
-  let news_style = css({
-    padding: '2px',
-    boxSizing:'border-box',
-    borderBottom:'4px solid pink',
-    ':hover':{
-      color:'teal',
-    },
-    '@media (max-width: 500px)':{
-      color:'Purple'
-    }
+  // let news_style = css({
+  //   padding: '2px',
+  //   boxSizing:'border-box',
+  //   borderBottom:'4px solid pink',
+  //   ':hover':{
+  //     color:'teal',
+  //   },
+  //   '@media (max-width: 500px)':{
+  //     color:'Purple'
+  //   }
 
-  })
+  // })
   
   return (
-    <div className= {`${news_style}`}>
+   
+    // <div className= {`${news_style}`}>
+    <div className={classes.News_item}>
       <div>
         <h3>{item.title}</h3>
         <div>
